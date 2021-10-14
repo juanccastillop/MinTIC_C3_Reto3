0ef8a392da084ad8b85c8a3d13705517cdc35d0e
Juan Pedreros <Juan.Pedreros@ilumno.com>
2021-10-13 23:37:46 -0500

    codigo primera parte funcional

---
+++ src/main/java/co/edu/usa/tufinca/TufincaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usa.tufinca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TufincaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TufincaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/co/edu/usa/tufinca/repository/FarmRepository.java
@@ -0,0 +1,9 @@
+package co.edu.usa.tufinca.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import co.edu.usa.tufinca.entidades.Farm;
+
+public interface FarmRepository extends CrudRepository<Farm,Long>{
+    
+}
